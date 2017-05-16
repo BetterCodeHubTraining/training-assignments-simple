27ccdc9bfa88837a9450610ea50ef6c497eaff59
rendra-code <rendra.code@gmail.com>
2017-05-16 10:54:14 +0700

    final object refactor
    final object refactor

--- src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
+++ src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
@@ -2,31 +2,27 @@ package eu.sig.training.ch05.buildandsendmail;
 
 public class BuildAndSendMail {
     // tag::buildAndSendMail[]
-    public void buildAndSendMail(MailMan m, MailAddress mailAddress, MailContent mailContent,MailFont font) {
+    public void buildAndSendMail(MailMan m, Mail mail) {
         // Format the email address
         
         // Format the message given the content type and raw message
-        MailMessage mMessage = formatMessage(font,mailContent.getMailMessage());
+        
         // Send message
-        m.send(mailAddress.getMailAddress(), mailContent.subject, mMessage);
+        m.send(mail.mailAddress, mail.mailContent.subject, mail.formatMessage());
     }
     // end::buildAndSendMail[]
 
     @SuppressWarnings("unused")
-    private MailMessage formatMessage(MailFont font, String string) {
-        return null;
-    }
+    
 
     private class MailMan {
 
         @SuppressWarnings("unused")
-        public void send(String mId, String subject, MailMessage mMessage) {}
+        public void send(MailAddress mailAddress, String subject, String message) {}
 
     }
 
-    private class MailFont {
-
-    }
+    
 
     private class MailMessage {
 
