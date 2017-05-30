ef7d18797b68853fc6f8b23a75221f9fd4f8ec19
Liam Blythe <l.blythe@sig.eu>
2017-05-30 15:03:10 +0200

    reduced length of long units

--- src/java/eu/sig/training/ch03/FlagFactory.java
+++ src/java/eu/sig/training/ch03/FlagFactory.java
@@ -10,15 +10,6 @@ public class FlagFactory {
     public List<Color> getFlagColors(Nationality nationality) {
         List<Color> result;
         switch (nationality) {
-        case DUTCH:
-            result = Arrays.asList(Color.RED, Color.WHITE, Color.BLUE);
-            break;
-        case GERMAN:
-            result = Arrays.asList(Color.BLACK, Color.RED, Color.YELLOW);
-            break;
-        case BELGIAN:
-            result = Arrays.asList(Color.BLACK, Color.YELLOW, Color.RED);
-            break;
         case UNCLASSIFIED:
         default:
             result = Arrays.asList(Color.GRAY);
